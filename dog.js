52ff166cd64857443787417fcf5da7d7256d1f6c
reedm26 <reedm26@yahoo.com>
2019-11-15 00:51:06 -0700

    pet counter working

--- dog.js
+++ dog.js
@@ -1,7 +1,7 @@
 let dogs = {
   huskey: {
     name: "Jeff",
-    moods: "jumpy",
+    moods: ["jumpy", "Psyco"],
     moodIndex: 0,
     tolerance: Math.floor(Math.random() * 9) + 1,
     img:
@@ -10,7 +10,7 @@ let dogs = {
   },
   lab: {
     name: "Leia",
-    moods: "Relaxed",
+    moods: ["Relaxed", "Nuts"],
     moodIndex: 0,
     tolerance: Math.floor(Math.random() * 9) + 1,
     img:
@@ -19,7 +19,7 @@ let dogs = {
   },
   poodle: {
     name: "Max",
-    moods: "",
+    moods: ["gracefull", "freak"],
     moodIndex: 0,
     tolerance: Math.floor(Math.random() * 9) + 1,
     img:
@@ -28,7 +28,44 @@ let dogs = {
   }
 };
 let activeDog = dogs.huskey;
+let dogToReset;
 let nameElem = document.querySelector("#dog-names");
-let moodsElem = document.querySelector("#dog-moods");
-let counterElem = document.querySelector("#pet-counter");
+let petsElem = document.querySelector("#dog-pets");
+let statusElem = document.querySelector("#dog-status");
 let imgElem = document.querySelector("#dog-pic");
+let petBtn = document.querySelector("#pet-button");
+
+function pet() {
+  activeDog.pets++;
+  if (activeDog.pets > activeDog.tolerance) {
+    activeDog.moodIndex = 1;
+  }
+  drawDog();
+}
+function drawDog() {
+  nameElem.innerText = activeDog.name;
+  petsElem.innerText = activeDog.pets;
+  statusElem.innerText = activeDog.moods[activeDog.moodIndex];
+  imgElem.setAttribute("src", activeDog.img);
+  if (activeDog.moodIndex > 0) {
+    petBtn.setAttribute("disabled", "true");
+    dogToReset = activeDog;
+    setTimeout(() => {
+      dogTreat();
+    }, 3000);
+  } else {
+    petBtn.removeAttribute("disabled");
+  }
+}
+function setActiveCat(dogName) {
+  activeDog = dogs[dogName];
+  drawDog();
+}
+
+function dogTreat() {
+  dogToReset.pets = 0;
+  dogToReset.moodIndex = 0;
+  drawDog();
+}
+
+drawDog();
